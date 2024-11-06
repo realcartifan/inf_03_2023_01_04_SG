f0eb7e8859925d4b2b9eff333badb7f8491c38ef
realcartifan <127745382+realcartifan@users.noreply.github.com>
2024-11-06 12:02:37 +0100

    Add files via upload

---
+++ zad4/wycieczki.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wycieczki po europie</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+<body>
+    <header></header>
+    <div id="list"></div>
+    <main>
+<div id="left">
+<h3>wycieczki na które są wolne miejcsa </h3>
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <h2>Besteselery </h2>
+<table>
+    <tr3>
+        <td2></td2>
+    </tr3>
+</table>
+</div>
+</ul>
+</div>
+<div id="center">
+<h2> nasze zdjecia</h2>
+<div id="right">
+<h2>skontaktuj sie </h2>
+<a href="mailto: turyst@dasdawd.pl">napisz do nas</a>
+</div>
+    </main>
+    <footer>tworzył 000000000000000000</footer>
+</body>
+</html>
